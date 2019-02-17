b6efa536d42b20ca7ee8f8355324ffed15522a20
BN <biczoxd@gmail.com>
2019-02-17 18:23:04 +0100

    Display the status of the downloading process.

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+
+	"golang.org/x/crypto/ssh/terminal"
 
 	"golang.org/x/net/html"
 )
@@ -18,6 +21,7 @@ import (
 type fileWriteCounter struct {
 	TotalSize uint64
 	Written   uint64
+	Channel   chan<- uint64
 }
 
 func (fwc *fileWriteCounter) Write(data []byte) (int, error) {
@@ -25,6 +29,8 @@ func (fwc *fileWriteCounter) Write(data []byte) (int, error) {
 	size := len(data)
 	// Increment the written bytes.
 	fwc.Written += uint64(size)
+	// Send it to the channel.
+	fwc.Channel <- fwc.Written
 
 	return size, nil
 }
@@ -100,9 +106,9 @@ func parseHTML(body string) ([]string, error) {
 	}
 }
 
-// Get the name of the track from the URL.
+// Get the name of the file from the URL.
 // Returns the name as a string.
-func getTrackName(URL string) string {
+func getFileName(URL string) string {
 	splittedURL := strings.Split(URL, "?dest-")
 	baseURL := splittedURL[0]
 	splittedBaseURL := strings.Split(baseURL, "/")
@@ -110,8 +116,7 @@ func getTrackName(URL string) string {
 }
 
 // Download the file from the URL.
-// Error is nil if no error occured.
-func downloadFile(URL string) (err error) {
+func downloadFile(URL string, fileName string, fileSizeChan chan<- uint64, writtenBytesChan chan<- uint64) {
 	// Get the data.
 	resp, err := http.Get(URL)
 	// Check error
@@ -125,6 +130,14 @@ func downloadFile(URL string) (err error) {
 	}
 	defer resp.Body.Close()
 
+	// Get the size of the file
+	fileSize, err := strconv.Atoi(resp.Header["Content-Length"][0])
+	if err != nil {
+		fileSizeChan <- 0
+	}
+	// then send it to the channel.
+	fileSizeChan <- uint64(fileSize)
+
 	// Check the download dir and if it isn't exists create it.
 	downloadDir := "Downloads"
 	err = os.Mkdir(downloadDir, os.ModePerm)
@@ -133,16 +146,44 @@ func downloadFile(URL string) (err error) {
 	}
 
 	// Create the file.
-	filePath := "Downloads/" + getTrackName(URL)
+	filePath := "Downloads/" + fileName
 	file, err := os.Create(filePath)
 	if err != nil {
 		return
 	}
 	defer file.Close()
 
-	_, err = io.Copy(file, resp.Body)
+	// Create the counter and
+	counter := fileWriteCounter{Channel: writtenBytesChan}
+	// start to copy the file.
+	_, err = io.Copy(file, io.TeeReader(resp.Body, &counter))
 
-	return
+	close(writtenBytesChan)
+}
+
+// Print the output to the standard output.
+func printStatus(fileSize uint64, writtenBytes uint64) {
+	// Function the convert the bytes to megabyte.
+	toKiloBytess := func(bytes uint64) uint64 {
+		return bytes / 1024
+	}
+
+	// Get the size of the terminal.
+	width, _, _ := terminal.GetSize(0)
+
+	// Get the status percentage.
+	percentage := int(writtenBytes * 100 / fileSize)
+	// Get the number of completed marks in the terminal UI.
+	markCount := percentage / 5
+
+	// Go back to the start of the current line.
+	fmt.Printf("\r")
+
+	// Create the line.
+	line := fmt.Sprintf("[%s%s] %d/%d KB", strings.Repeat("=", markCount), strings.Repeat(".", 20-markCount), toKiloBytess(writtenBytes), toKiloBytess(fileSize))
+
+	// Print it and fill the remaining space with spaces.
+	fmt.Printf("%s%s", line, strings.Repeat(" ", width-len(line)))
 }
 
 func main() {
@@ -160,9 +201,31 @@ func main() {
 		log.Fatal("Error while parsing the HTML: " + err.Error())
 	}
 
-	err = downloadFile(links[0])
-	if err != nil {
-		log.Fatal("Cannot download the file: " + err.Error())
+	fileSizeChan := make(chan uint64)
+	// Download the files from the links one-by-one sequentially.
+	for i, link := range links {
+		writtenBytes := make(chan uint64)
+		// Get the name of the file from the URL.
+		fileName := getFileName(link)
+		// Start the file download.
+		go downloadFile(link, fileName, fileSizeChan, writtenBytes)
+
+		fmt.Printf("[%d/%d] %s (%s)\n", i+1, len(links), fileName, link)
+
+		// Get the size of the file.
+		fileSize := <-fileSizeChan
+
+		// While the channel is open
+		for {
+			writtenBytes, more := <-writtenBytes
+			if !more {
+				break
+			}
+
+			printStatus(fileSize, writtenBytes)
+		}
+		// New line after complete.
+		fmt.Println()
 	}
 
 	fmt.Println("DONE")
